2dc028a603f2f1f05d4522d670094525de4af7d7
j-o-d-o <jdobler@protonmail.com>
2019-12-03 20:58:37 +0100

    install files

--- test_build.sh
+++ test_build.sh
@@ -2,9 +2,15 @@
 
 ROOTDIR=`pwd`
 
+rm -rf build
+rm -rf dist
+
 DISTFOLDER=$ROOTDIR/dist
 BUILDFOLDER=$ROOTDIR/build
 mkdir -p $BUILDFOLDER
 cd $BUILDFOLDER
 
 cmake -DCMAKE_INSTALL_PREFIX=$DISTFOLDER $ROOTDIR || exit 1
+
+make 
+make install
